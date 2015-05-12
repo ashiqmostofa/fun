8ad76bbb0e52af9118fe34b17fd82d0f73221a96
cacophonix <ashiqulmostofa@gmail.com>
2015-05-12 16:39:17 +0600

    .gitignore added

--- archive/2015.05/2015.05.12 - TopCoder Open Round #1C/DevuAndPlantingTrees.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class DevuAndPlantingTrees {
-    public int maximumTreesDevuCanGrow(String[] garden) {
-        return 0;
-    }
-}
--- src/main/DevuAndPlantingTrees.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class DevuAndPlantingTrees {
-    public int maximumTreesDevuCanGrow(String[] garden) {
-        return 0;
-        
-    }
-}
